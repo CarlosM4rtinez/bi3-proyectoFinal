1d18329dfaa6ccf62106891924f2c51434e322fc
CarlosM4rtinez <inf.carlosmartinez@gmail.com>
2019-05-01 14:25:03 -0500

    base del proyecto

---
+++ src/main/java/Servicios/ReglasAsociacionServices.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servicios;
+
+import java.io.InputStream;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+/**
+ * Clase encargada de los servicios de reglas de asociacion
+ * @author Kevin Zapata & Carlos Martinez
+ */
+@Path("data-mining/reglas-asociacion")
+public class ReglasAsociacionServices extends Application{
+
+    @POST
+    @Path("/consumir")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String asociarCuenta(@FormDataParam("file") InputStream archivo, @FormDataParam("file") FormDataContentDisposition archivoDetalles) {
+        return null;
+                
+    }
+
+}
