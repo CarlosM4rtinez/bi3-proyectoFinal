6796f3e3bf41beaa23a3c54c833d4deba22c42f2
CarlosM4rtinez <inf.carlosmartinez@gmail.com>
2019-05-01 14:26:03 -0500

    base del proyecto

---
+++ src/main/java/Servicios/ApplicationConfig.java
@@ -0,0 +1,37 @@
+package Servicios;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ * Clase que configura la aplicacion para el uso de servicios
+ * @author Kevin Zapata & Carlos Martinez
+ */
+@javax.ws.rs.ApplicationPath("services")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Agrega los servicios a los resources
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(Servicios.ReglasAsociacionServices.class);
+    }
+    
+}
