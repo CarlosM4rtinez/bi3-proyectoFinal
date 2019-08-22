c4df83cc06332b4a5ba91cf0011168362d1d043e
Carlos Martinez <Carlos Martinez@192.168.1.51>
2019-08-22 12:51:04 -0500

    Menu dinamico reglas de asociacion

--- src/main/webapp/js/functions.js
+++ src/main/webapp/js/functions.js
@@ -1,9 +1,35 @@
 /* 
  * Carlos Martinez
  */
-
+/**
+ * Returna el valor de una variable por get
+ * @param {String} key Nombre del parametro get
+ * @returns si se encuentra retorna el valor de la variable, de lo contrario null
+ * @author Johnny Alexander Salazar
+ * @version 0.1
+*/
+function getUrlParametro(key){
+    var url = decodeURIComponent(window.location.search.substring(1)),
+        parametros = url.split('&'),
+        claves;
+    for (var i = 0; i < parametros.length; i++) {
+        claves = parametros[i].split('=');
+        if (claves[0] === key) {
+            // retornamos el valor
+            return claves[1];
+        }
+    }
+    return null;
+}
 // Una vez cargado la pagina ejecutamos las acciones
 $(document).ready(function(){
+    // Validamos si hay un algoritmo seleccionado
+    var algoritmo = getUrlParametro('algoritmo');
+    // agregamos la clase active al menu
+    $("#algoritmo"+algoritmo).addClass("active");
+    // asignamos el valor al select de opciones
+    $("#algoritmo").val(algoritmo);
+    
     // Consumimos los servicios por ajax de jquery
     $(document).on('submit','#form-mineria',function(e){
         // ocultamos el conjunto de datos
@@ -103,6 +129,14 @@ $(document).ready(function(){
         restart();
         $("#graphBIG").removeClass("fixed").html("");
         $(this).remove();
+    });
+    // Cambiamos el menu, dependiendo de la opcion seleccionada
+    $("body").on("change","#algoritmo", function(){
+        var algoritmo = $(this).val();
+        // Eliminamos el active en el menu del anterior algoritmo
+        $("#algoritmo1,#algoritmo2,#algoritmo3").removeClass("active");
+        // Asignamos el active en el menu del algoritmo seleccionado 
+        $("#algoritmo"+algoritmo).addClass("active");
     });
 });
 
